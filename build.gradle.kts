dd7fb50ff7a04dce2d6eb445c71967067829cf37
Jeffrey Kuo <kuojeffrey@gmail.com>
2023-08-17 15:05:20 -0400

    remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android") version "1.8.21"
+    `maven-publish`
+    signing
 }
 
 val pkg: String = "edu.cmu.cs.sinfonia"
+version = "1.0.20230818"
 
 android {
     namespace = pkg
@@ -48,7 +51,6 @@ dependencies {
     implementation("com.wireguard.android:tunnel:1.0.20230427")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.test:monitor:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -59,4 +61,44 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
     implementation("androidx.security:security-crypto:1.0.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = pkg
+            artifactId = "sinfonia-tier3"
+            version = version
+            afterEvaluate {
+                from(components["release"])
+            }
+            pom {
+                name.set("Sinfonia Tier 3 Android Library")
+                description.set("Sinfonia tier 3 library for edge-native Android applications")
+                developers {
+                    organization {
+                        name.set("Carnegie Mellon University")
+                    }
+                    developer {
+                        name.set("Jeffrey Kuo")
+                        email.set("luyok@andrew.cmu.edu")
+                    }
+                    developer {
+                        name.set("Jan Harkes")
+                        email.set("jaharkes@cs.cmu.edu")
+                    }
+                }
+            }
+        }
+    }
+//    repositories {
+//        maven {
+//            name = "sonatype"
+//        }
+//    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
